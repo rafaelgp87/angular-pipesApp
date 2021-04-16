7b05972ef93416722c41320d115acd41b725aac3
Rafael Gutiérrez <rafaelgp87@gmail.com>
2021-04-16 15:15:28 -0500

    Pruebas de pipes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  
+  nombre: string = 'raFael gUtiéRreZ';
+  valor : number = 1000;
+
+  mostrarNombre() {
+    console.log( this.nombre );
+    console.log( this.valor );
+  }
+
 }
